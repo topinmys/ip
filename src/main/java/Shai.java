af63a1f24ed8fdd18aa22ced749645e42fd525ce
Guo Jie <ygj060819@gmail.com>
2026-08-20 14:54:11 +0800

    Shai.java: Update code to echo commands

--- src/main/java/Shai.java
+++ src/main/java/Shai.java
@@ -1,17 +1,42 @@
+import java.util.Scanner;
+
+/**
+ * A simple command-line chatbot that echoes commands until the user says goodbye.
+ */
 public class Shai {
+    private static final String LINE = "\t____________________________________________________________";
+
+    /**
+     * Starts Shai, displays the greeting, and processes commands from standard input.
+     *
+     * @param args command-line arguments (not used)
+     */
     public static void main(String[] args) {
-        String banner = "____________________________________________________________\n"
-                + "  ____  _           _\n"
-                + " / ___|| |__   __ _(_)\n"
-                + " \\___ \\| '_ \\ / _` | |\n"
-                + "  ___) | | | | (_| | |\n"
-                + " |____/|_| |_|\\__,_|_|\n"
-                + "Yo, what's good. I'm Shai.\n"
-                + "Drop the word, I gotchu.\n"
-                + "____________________________________________________________\n"
-                + "Say less. Stay blessed, peace!\n"
-                + "____________________________________________________________\n";
+        String banner = LINE + "\n"
+                + "\t  ____  _           _\n"
+                + "\t / ___|| |__   __ _(_)\n"
+                + "\t \\___ \\| '_ \\ / _` | |\n"
+                + "\t  ___) | | | | (_| | |\n"
+                + "\t |____/|_| |_|\\__,_|_|\n"
+                + "\tYo, what's good. I'm Shai.\n"
+                + "\tDrop the word, I gotchu.\n"
+                + LINE;
 
         System.out.println(banner);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            System.out.println(LINE);
+
+            if (command.equals("bye")) {
+                System.out.println("\tSay less. Stay blessed, peace!");
+                System.out.println(LINE);
+                break;
+            }
+
+            System.out.println("\t" + command);
+            System.out.println(LINE);
+        }
     }
 }
