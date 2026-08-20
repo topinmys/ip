5e68d2519c22c5ff6ea680937bd64b28213b75e2
Guo Jie <ygj060819@gmail.com>
2026-08-20 13:48:52 +0800

    Shai: Rename Duke chatbot and update startup banner

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Shai User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Shai.java
@@ -0,0 +1,10 @@
+public class Shai {
+    public static void main(String[] args) {
+        String banner = "  ____  _           _\n"
+                + " / ___|| |__   __ _(_)\n"
+                + " \\___ \\| '_ \\ / _` | |\n"
+                + "  ___) | | | | (_| | |\n"
+                + " |____/|_| |_|\\__,_|_|\n";
+        System.out.println(banner);
+    }
+}
