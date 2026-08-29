d37feb49501e7a1539f6d45a445938f9f9334ceb
Guo Jie <ygj060819@gmail.com>
2026-08-29 16:15:31 +0800

    Add Javadoc for ShaiException constructor

--- src/main/java/shai/exception/ShaiException.java
+++ src/main/java/shai/exception/ShaiException.java
@@ -2,6 +2,7 @@ package shai.exception;
 
 /** Represents an expected application error that can be shown to the user. */
 public class ShaiException extends Exception {
+    /** Creates an application exception with the specified message. */
     public ShaiException(String message) {
         super(message);
     }
